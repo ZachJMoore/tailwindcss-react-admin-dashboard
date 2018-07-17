2fe11a64864fa31ff680582cd8c48fd6c521edb6
ZachMoore <Get3Moore@gmail.com>
2018-07-17 15:01:01 -0400

    Add notifications

---
+++ src/components/notifications/NotifBasic.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+let NotifBasic = props => (
+    <div className={"flex flex-col justify-between w-full p-2 py-4 pr-8 my-2 shadow-md overflow-hidden rounded relative " + props.className}>
+        <div className="flex items-center">
+            {props.icon && <i className={"text-2xl mr-2 " + props.icon}></i>}
+            <span>
+                {props.children}
+            </span>
+        </div>
+        <div className="flex">
+            {props.buttons && props.buttons.map((button, index)=><button className={"py-1 px-4 mr-2 mt-2 border rounded-full hover:bg-grey-light " + button.className} onClick={button.onClick} key={index}>{button.children}</button>)}
+        </div>
+        <div>
+            {props.close && <button onClick={props.close} className="flex items-center justify-center rounded-full hover:bg-grey-light h-6 w-6 absolute pin-r pin-t m-1" style={{color: "inherit"}} ><i className="fas fa-times"></i></button>}
+        </div>
+    </div>
+)
+
+export default NotifBasic;
--- src/components/pages/Notifications.js
+++ src/components/pages/Notifications.js
@@ -1,8 +1,65 @@
 import React, { Component } from 'react';
+import BreadCrumb from '../navigation/BreadCrumb';
+import NotifBasic from '../notifications/NotifBasic';
 
 class Notifications extends Component {
   render() {
-    return (<div>Notifications</div>);
+    return (<>
+      <BreadCrumb />
+      <div className="w-full flex flex-wrap my-2">
+        <div className="w-full md:w-1/2 px-2">
+          <span className="text-xl">Basic Notifications</span>
+          <NotifBasic className="text-grey-darker">
+            This is a basic notification.
+          </NotifBasic>
+          <NotifBasic className="text-grey-darker" close={()=>{console.log("notification closed")}}>
+            This is a basic notification with a close button.
+          </NotifBasic>
+          <NotifBasic className="text-grey-darker" close={()=>{console.log("notification closed")}}>
+            Notifications can also be multiple lines. Although, it may defeat the purpose of a notification if they begin to get too large. But the option is entirely up to you.
+          </NotifBasic>
+          <NotifBasic className="text-grey-darker" close={()=>{console.log("notification closed")}} icon="fas fa-comments">
+            Icons can be used to add interest
+          </NotifBasic>
+          <NotifBasic className="text-grey-darker" close={()=>{console.log("notification closed")}} buttons={[{children: "yes", onClick:()=>{console.log("button pressed")}}, {children: "no", onClick: ()=>{console.log("button pressed")}}]}>
+            Button options can also be passed to make actionable notifications.
+          </NotifBasic>
+        </div>
+        <div className="w-full md:w-1/2 px-2">
+          <span className="text-xl">Colored Notifications</span>
+          <NotifBasic className="text-white bg-grey-dark" close={()=>{console.log("notification closed")}}>
+            .text-white .bg-grey-dark
+          </NotifBasic>
+          <NotifBasic className="text-white bg-red-light" close={()=>{console.log("notification closed")}}>
+            .text-white .bg-red-light
+          </NotifBasic>
+          <NotifBasic className="text-white bg-orange-dark" close={()=>{console.log("notification closed")}}>
+            .text-white .bg-orange-dark
+          </NotifBasic>
+          <NotifBasic className="text-grey-darkerest bg-yellow" close={()=>{console.log("notification closed")}}>
+            .text-grey-darkerest .bg-yellow
+          </NotifBasic>
+          <NotifBasic className="text-white bg-green-dark" close={()=>{console.log("notification closed")}}>
+            .text-white .bg-green-dark
+          </NotifBasic>
+          <NotifBasic className="text-white bg-teal-darker" close={()=>{console.log("notification closed")}}>
+            .text-white .bg-teal-dark
+          </NotifBasic>
+          <NotifBasic className="text-grey-darkerest bg-blue-lighter" close={()=>{console.log("notification closed")}}>
+            .text-grey-darkerest .bg-blue-lighter
+          </NotifBasic>
+          <NotifBasic className="text-white bg-indigo-dark" close={()=>{console.log("notification closed")}}>
+            .text-white .bg-indigo-dark
+          </NotifBasic>
+          <NotifBasic className="text-white bg-purple-dark" close={()=>{console.log("notification closed")}}>
+            .text-white .bg-purple-dark
+          </NotifBasic>
+          <NotifBasic className="text-white bg-pink-dark" close={()=>{console.log("notification closed")}}>
+            .text-white .bg-pink-dark
+          </NotifBasic>
+        </div>
+      </div>
+    </>);
   }
 }
 
