207f5337d62a43b320490ba42ce5d2321e0d669a
ZachMoore <Get3Moore@gmail.com>
2018-07-17 15:04:45 -0400

    Opt out of service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './tailwind.css';
 import App from './components/App';
 import { BrowserRouter } from "react-router-dom";
-import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
-registerServiceWorker();
+unregister();
